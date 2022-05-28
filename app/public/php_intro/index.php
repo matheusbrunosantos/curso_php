3909b8af46c82ab6e751a342fb5b4217ec51fbb4
matheus santos <matheusbsnt@gmail.com>
2022-05-27 23:35:03 -0300

    commit inicial

---
+++ app/public/php_intro/index.php
@@ -0,0 +1,39 @@
+<?php
+
+$categorias = [];
+$categorias[] = 'infantil';
+$categorias[] = 'adolescente';
+$categorias[] = 'adulto';
+
+//print_r($categorias);
+
+$nome = 'Eduardo';
+$idade = 15;
+
+//var_dump($nome);
+//var_dump($idade);
+
+if($idade >= 6 && $idade <= 12) 
+{
+    for($i = 0; $i <= count($categorias); $i++)
+    {
+        if($categorias[$i] == 'infantil')
+        echo "O nadador ".$nome. " compete na categoria " .$categorias[$i];
+    }
+}
+else if($idade >= 13 && $idade <= 18)
+{
+    for($i = 0; $i <= count($categorias); $i++)
+    {
+        if($categorias[$i] == 'adolescente')
+        echo "O nadador ".$nome. " compete na categoria adolescente";
+    }
+}
+else
+{ 
+    for($i = 0; $i <= count($categorias); $i++)
+    {
+        if($categorias[$i] == 'adulto')
+        echo "O nadador ".$nome. " compete na categoria adulto";
+    }
+}
